33edc65151b413a96d9360cbba508ddf44b5cca2
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-07 21:04:11 -0500

    seed users

---
+++ tasks/seed_users.js
@@ -0,0 +1,102 @@
+export const users = [
+  {
+    "username": "user1",
+    "email": "user1@example.com",
+    "password": "pass123",
+    "bio": "Just a regular user.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "coolguy87",
+    "email": "coolguy87@example.com",
+    "password": "coolPass456",
+    "bio": "Living life to the fullest!",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "codingmaster",
+    "email": "code@example.com",
+    "password": "codePass",
+    "bio": "Passionate about coding.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "bookworm23",
+    "email": "booklover@example.com",
+    "password": "bookPass123",
+    "bio": "In love with books!",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "traveler123",
+    "email": "travel@example.com",
+    "password": "travelPass",
+    "bio": "Exploring the world one place at a time.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "fitnessfanatic",
+    "email": "fitness@example.com",
+    "password": "fitPass789",
+    "bio": "Stay fit, stay healthy!",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "musicjunkie",
+    "email": "musiclover@example.com",
+    "password": "musicPass",
+    "bio": "All about that music life.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "photographer22",
+    "email": "photo@example.com",
+    "password": "photoPass",
+    "bio": "Capturing moments through the lens.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "foodie88",
+    "email": "foodlover@example.com",
+    "password": "foodiePass",
+    "bio": "Exploring the culinary world!",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  },
+  {
+    "username": "gamerpro99",
+    "email": "gamer@example.com",
+    "password": "gamerPass",
+    "bio": "Leveling up in the gaming world.",
+    "profilePicture": "happy_cat.jpg",
+    "friends": [],
+    "userComments": [],
+    "userPosts": []
+  }
+]

729ab2a0933699fd865a49aa652561787ccf840a
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-08 16:48:50 -0500

    more seed

---
+++ tasks/seed_music.js
@@ -0,0 +1,141 @@
+export const MBDTF_songs = [
+  {
+    "name": "Dark Fantasy",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Gorgeous",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "POWER",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "All of the Lights",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Monster",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "So Appalled",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Devil in a New Dress",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Runaway",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Hell of a Life",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Blame Game",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Lost in the World",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  },
+  {
+    "name": "Who Will Survive in America",
+    "artist": "Kanye West",
+    "genre": "hip-hop"
+  }
+];
+export const Grad_songs = [
+  {
+    "name": "Good Morning",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Champion",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Stronger",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "I Wonder",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Good Life",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Can't Tell Me Nothing",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Barry Bonds",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Drunk and Hot Girls",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Flashing Lights",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Everything I Am",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "The Glory",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Homecoming",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  },
+  {
+    "name": "Big Brother",
+    "artist": "Kanye West",
+    "genre": "Hip-Hop"
+  }
+];
+
+export const MBDTF = {
+  name: "My Beautiful Dark Twisted Fantasy",
+  artist: "Kanye West",
+  genre: "hip-hop"
+};
+
+export const Graduation= {
+  name: "Graduation",
+  artist: "Kanye West",
+  genre: "hip-hop"
+}
---
+++ tasks/seed_posts.js
@@ -0,0 +1,66 @@
+export const song_posts =
+[
+  {
+    "rating": 4,
+    "content": "Absolutely love this song! It's a masterpiece."
+  },
+  {
+    "rating": 3,
+    "content": "Decent track, but not my favorite. It's alright."
+  },
+  {
+    "rating": 5,
+    "content": "A perfect 5! This song is pure gold. Can't get enough of it."
+  },
+  {
+    "rating": 2,
+    "content": "Not my cup of tea. The song lacks appeal to me."
+  },
+  {
+    "rating": 4,
+    "content": "Solid song, enjoyable. I would recommend it."
+  },
+  {
+    "rating": 1,
+    "content": "Honestly, I don't like this song at all. It's a miss for me."
+  },
+  {
+    "rating": 3,
+    "content": "Good vibes overall. I find myself humming along."
+  },
+  {
+    "rating": 2,
+    "content": "Meh, it's okay. Not great, not terrible."
+  },
+  {
+    "rating": 4,
+    "content": "Really feeling this song! Great beats and lyrics."
+  },
+  {
+    "rating": 1,
+    "content": "Not impressed. I expected more from this song."
+  },
+  {
+    "rating": 4,
+    "content": "Kanye popped off with this one!"
+  },
+  {
+    "rating": 3,
+    "content": "Not his best work in my opinion..."
+  },
+  {
+    "rating": 5,
+    "content": "yeezy yeezy yeezy just jumped over jumpman!"
+  }
+];
+
+export const album_posts = [
+  {
+    "rating": 4,
+    "content": "Probably his best work after 'The Life of Pablo'."
+  },
+  {
+    "rating": 4,
+    "content": "He made Graduation"
+  }
+]

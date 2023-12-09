3476f41249bb74df02683f903dff3568c8da453b
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-09 15:15:56 -0500

    register routes done

--- routes/register.js
+++ routes/register.js
@@ -1,57 +1,57 @@
 import {Router} from 'express';
 import {createUser} from '../data/users.js';
-import * as validation from '../validation.js';
-
+import * as val from '../validation.js';
 
 const router = Router();
-
 
 router
   .route('/register')
   .get(async (req, res) => {
-    //code here for GET
-    //render view of registration form
-    if (req.session.user) {
-      res.redirect('/posts')
-    }
-    else {
-      res.render('register');
-    }
+    res.render('register');
   })
   .post(async (req, res) => {
-    //code here for POST
-    const { userNameInput, emailAddressInput, passwordInput, confirmPasswordInput, bioInput, profilePictureInput} = req.body;
+    let formInput = req.body;
 
     // Check if all fields are supplied
     const missingFields = [];
-    if (!userNameInput) missingFields.push('Username');
-    if (!emailAddressInput) missingFields.push('Email Address');
-    if (!passwordInput) missingFields.push('Password');
-    if (!confirmPasswordInput) missingFields.push('Confirm Password');
-    if (!bioInput) missingFields.push('Bio');
-    if (!profilePictureInput) missingFields.push('Profile Picture');
+    if (!formInput.usernameInput) missingFields.push('Username');
+    if (!formInput.emailAddressInput) missingFields.push('Email Address');
+    if (!formInput.passwordInput) missingFields.push('Password');
+    if (!formInput.confirmPasswordInput) missingFields.push('Confirm Password');
+    if (!formInput.bioInput) missingFields.push('Bio');
+    if (!formInput.profilePictureInput) missingFields.push('Profile Picture');
 
     if (missingFields.length > 0) {
-        return res.status(400).render('register', { error: `Missing field(s): ${missingFields.join(', ')}` });
-    }
-    
-    if (passwordInput !== confirmPasswordInput) {
-        return res.status(400).render('register', { error: 'Passwords do not match' });
-    }
-
-    let check;
-    try {
-        check = registerValidate(emailAddressInput, passwordInput, bioInput);
-    }
-    catch (e) {
-        return res.status(400).render('register', { error: e });
-    }
-    if (check !== true) {
-        return res.status(400).render('register', { error: check });
+      return res.status(400).render('register', {
+        title: 'Register',
+        hasErrors: true,
+        error: `Missing field(s): ${missingFields.join(', ')}`
+      });
     }
 
     try {
-        const user = await createUser(userNameInput, emailAddressInput, passwordInput, [], [], [], bioInput, profilePictureInput);
+      formInput.usernameInput = val.checkUsername(formInput.usernameInput);
+      formInput.emailAddressInput = val.checkEmail(formInput.emailAddressInput);
+      formInput.passwordInput = val.checkPass(formInput.passwordInput);
+      formInput.confirmPasswordInput = val.confirmPass(formInput.passwordInput, formInput.confirmPasswordInput);
+      formInput.bioInput = val.checkBio(formInput.bioInput);
+      formInput.profilePictureInput = val.checkProfilePic(formInput.profilePictureInput);
+    } catch (e) {
+      return res.status(400).render('register', {
+        title: 'Register',
+        hasErrors: true,
+        error: e
+      })
+    }
+
+    try {
+        const user = await createUser(
+            formInput.usernameInput,
+            formInput.emailAddressInput,
+            formInput.passwordInput,
+            [],
+            []
+        );
         if (user) { 
           return res.status(200).redirect('/login');
         }

2cc35bc187c57fe87bc3ae3286e14e9bf9e35e8a
Yash Sharma <94649384+ysharma72@users.noreply.github.com>
2023-12-09 21:15:35 -0500

    redoing post routes for functionality

--- routes/posts.js
+++ routes/posts.js
@@ -1,40 +1,44 @@
 import {Router} from 'express';
 const router = Router();
-import {postsData, usersData} from '../data/index.js';
-import * as validation from '../validation.js';
-import {fromPostman} from "../helpers.js";
-import xss from 'xss';
+import {postsData} from '../data/index.js';
+import validation from '../validation.js';
 
 
-// ALL ROUTES REQUIRE USER TO BE LOGGED IN
-// GET request / to see all posts
-// GET request: /:id to see a specific post (edit button shows up if you own it!)
-// PATCH request: /:id to like a post
-
-// THESE ROUTES REQUIRE A USER TO OWN THE POST
-// DELETE request: /:id to delete a specific post
-// GET request: /:id/edit render the form to edit a post
-// PATCH request: /:id/edit modify the rating/content of a post
-
+// under music: /music/:id GET request to see all posts under a song ---- /music/:id POST request to create a new post under a song
+// GET request: /posts/:id to see a specific post
+// DELETE request: /posts/:id to delete a specific post
+// PATCH request: /posts/:id to like a post
+// GET request /posts/:id/edit render the form to edit a post -> then PATCH request with the data that needs to be edited (content of post)
+// GET request /posts to see all posts from friends
 
 router
-  .route('/')
+  .route('/music/:id')
   .get(async (req, res) => {
     // GET / gets all posts
     try {
-      // if (!req.session.user) res.render('/login');
-      const posts = await postsData.getAllPosts();
-      if (posts === undefined)
-        return res.status(500).json({error: "Internal Server Error"});
-      if (fromPostman(req.headers['user-agent'])) return res.json({posts: posts});
-      res.render('posts/all', {posts: posts});
+      req.params.id = validation.checkId(req.params.id, 'music id');
+      const postList = await postsData.getAll(req.params.id);
+      res.render('posts/index', {posts: postList});
     } catch (e) {
-      res.status(404).json({error: e});
+      res.status(500).json({error: e});
+    }
+  })
+  .post(async (req, res) => {
+    const {song_id, rating, user_id, content} = req.body;
+    let errors = [];
+    if (!song_id) throw 'You must provide a song id';
+    try {
+      req.params.id = 
+      const date = new Date();
+      const newPost = await postsData.createPost(song_id, rating, user_id, content, date, 0);
+      res.redirect(`/posts/${newPost._id}`);
+    } catch (e) {
+      res.status(500).json({error: e});
     }
   })
 
 router
-  .route('/:id')
+  .route('/posts/:id')
   .get(async (req, res) => {
     // GET /:id gets a specific post
 
@@ -130,86 +134,6 @@ router
       if (fromPostman(req.headers['user-agent'])) return res.json({deleted: deletedPost});
       res.redirect('/posts');
     } catch (e) {
-      let status = e[0];
-      let message = e[1];
-      res.status(status).render("error",{error: message, link:`/posts/`});
-    }
-  });
-
-
-router
-  .route('/:id/edit')
-  .get(async (req, res) => {
-    // GET /:id/edit renders the form to edit a specific post (only if the user owns it)
-
-    // let user_id;
-    try {
-      // if (!req.session.user) res.render('/login');
-      // user_id = validation.checkId(req.session.user._id, 'User ID');
-      req.params.id = validation.checkId(req.params.id, 'Post ID');
-    } catch (e) {
-      return res.status(400).render("error",{error: e, link:`/posts/`});
-    }    
-
-    try {
-      const ownPost = await usersData.userOwnsPost(req.session.user._id, req.params.id)
-      if (ownPost === undefined)
-        return res.status(500).render("error",{error: "Internal Server Error", link:`/posts/`});
-      if (!ownPost) {
-        return res.status(403).render("error",{error: "You do not have permission to edit this post.", link:`/posts/`});
-      }
-      const post = await postsData.getPostById(req.params.id);
-      if (post === undefined)
-        return res.status(500).render("error",{error: "Internal Server Error", link:`/posts/`});
-      if (fromPostman(req.headers['user-agent'])) return res.json({post: post});
-      res.render('posts/edit', {post: post});
-    } catch (e) {
-      return res.status(404).render("error",{error: e, link:`/posts/`});
-    }
-  })
-
-
-  .patch(async (req, res) => {
-    // PATCH /:id/edit modifies the rating/content of a specific post (only if the user owns it)
-    for (let key of Object.keys(req.body)) {
-      req.body[key] = xss(req.body[key])
-    }
-    let {rating, content} = req.body;
-    if (!rating)
-      return res.status(400).render("error",{error: 'must provide a rating!', link:`/posts/`});
-    if (!content)
-      return res.status(400).render("error",{error: 'must provide post content!', link:`/posts/`});
-    try {
-      // if (!req.session.user) res.render('/login');
-      // const user_id = validation.checkId(req.session.user._id, 'User ID');
-      req.params.id = validation.checkId(req.params.id, 'Post ID');
-      rating = validation.checkRating(rating, 'rating');
-      content = validation.checkString(content, 'content');     
-    } catch (e) {
-      return res.status(400).render("error",{error: e, link:`/posts/`});
-    }
-
-    let alreadyLiked;
-    try {
-      const ownPost = await usersData.userOwnsPost(req.session.user._id, req.params.id)
-      alreadyLiked = await usersData.alreadyLikedPost(req.session.user._id, req.params.id)
-      if (ownPost === undefined || alreadyLiked === undefined)
-        return res.status(500).render("error",{error: "Internal Server Error", link:`/posts/`});
-      if (!ownPost) {
-        return res.status(403).render("error",{error: "You do not have permission to edit this post.", link:`/posts/`});
-      }
-    } catch (e) {
-      return res.status(404).render("error",{error: e, link:`/posts/`});
-    }
-
-    try {
-      const editedPost = await postsData.editPostContent(req.params.id, rating, content);
-      if (editedPost === undefined)
-        return res.status(500).render("error",{error: "Internal Server Error", link:`/posts/`});
-      return res.json({post: editedPost, ownPost: true, alreadyLiked: alreadyLiked});
-      res.render('posts/single', {post: editedPost, ownPost: true, alreadyLiked: alreadyLiked});
-    }
-    catch (e) {
       let status = e[0];
       let message = e[1];
       res.status(status).render("error",{error: message, link:`/posts/`});

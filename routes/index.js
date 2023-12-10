21c226d9335f38b209108efd136b81ccac19be12
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-10 07:49:46 -0500

    added users route

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ import logoutRoutes from './logout.js';
 import postRoutes from './posts.js';
 import registerRoutes from './register.js';
 
+let userRoutes = 'cock';
 
 const constructorMethod = (app) => {
   app.use('/login', loginRoutes);
   app.use('/logout', logoutRoutes);
   app.use('/register', registerRoutes);
   app.use('/posts', postRoutes);
+  app.use('/users', userRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Route not found'});

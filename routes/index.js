b81e13c43d6ba94c98f1ec1fdbca758389c86d23
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-11 13:26:08 -0500

    home routes added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import postRoutes from './posts.js';
 import registerRoutes from './register.js';
 import userRoutes from './users.js';
 import homeRoutes from './home.js';
+import musicRoutes from './music.js';
 
 const constructorMethod = (app) => {
   app.use('/login', loginRoutes);
@@ -12,6 +13,7 @@ const constructorMethod = (app) => {
   app.use('/posts', postRoutes);
   app.use('/users', userRoutes);
   app.use('/home', homeRoutes);
+  app.use('/music', musicRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Route not found'});

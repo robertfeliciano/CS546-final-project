603c2e168dd02e1baf6f23a428f7251152315429
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-12 10:41:47 -0500

    more index

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import registerRoutes from './register.js';
 import userRoutes from './users.js';
 import homeRoutes from './home.js';
 import musicRoutes from './music.js';
+import commentRoutes from './comments.js';
 
 const constructorMethod = (app) => {
   app.use('/login', loginRoutes);
@@ -14,6 +15,7 @@ const constructorMethod = (app) => {
   app.use('/users', userRoutes);
   app.use('/home', homeRoutes);
   app.use('/music', musicRoutes);
+  app.use('/comments', commentRoutes);
   // TODO need error routes
 
   app.use('*', (req, res) => {

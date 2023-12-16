0be5c0d1f207f9f023efba40d7cfa04731b192d7
robertfeliciano <robertfeliciano314@gmail.com>
2023-12-16 10:14:27 -0500

    more data passed from routes to handlebars

--- routes/comments.js
+++ routes/comments.js
@@ -26,18 +26,18 @@ router
 
       req.body.content = validation.checkString(xss(req.body.content), 'Comment Content');
     } catch (e) {
-      return res.status(400).render("error",{error: e, link:`/posts/`});
+      return res.status(400).render("error",{userInfo: req.session.user, error: e, link:`/posts/`});
     }
 
     try {
       const comment = await commentsData.createComment(req.params.id, req.session.user._id, req.body.content, new Date());
       if (comment === undefined)
-        return res.status(500).render("error",{error: "Internal Service Error", link:`/posts/${req.params.id}`});
+        return res.status(500).render("error",{userInfo: req.session.user, error: "Internal Service Error", link:`/posts/${req.params.id}`});
       // redirect to GET /comments/:new_comment_id to view the comment you made
       res.redirect(`/comments/${comment._id}`);
     } catch (e) {
       console.log(e);
-      res.status(500).render("error",{error: e, link:`/posts/${req.params.id}`});
+      res.status(500).render("error",{userInfo: req.session.user, error: e, link:`/posts/${req.params.id}`});
     }
   })
   .get(async (req, res) => {
@@ -49,24 +49,24 @@ router
 
       req.params.id = validation.checkId(req.params.id, 'Comment ID');
     } catch (e) {
-      return res.status(400).render("error",{error: e, link:`/posts/`});
+      return res.status(400).render("error",{userInfo: req.session.user, error: e, link:`/posts/`});
     }
 
     try {
       const comment = await commentsData.getCommentById(req.params.id);
       if (comment === undefined)
-        return res.status(500).render("error",{error: "Internal Service Error", link:`/posts/`});
+        return res.status(500).render("error",{userInfo: req.session.user, error: "Internal Service Error", link:`/posts/`});
 
       const commenterId = comment.user_id;
       const user_id = req.session.user._id;
       let ownComment = commenterId.equals(user_id);
       if (fromPostman(req.headers['user-agent']))
         return res.json({comment: comment, ownComment: ownComment})
-      res.render('comments/single', {comment: comment, ownComment: ownComment});
+      res.render('comments/single', {userInfo: req.session.user, comment: comment, ownComment: ownComment});
 
     } catch (e) {
       // only error caught would be if db cant find comment
-      res.status(404).render("error",{error: e, link:`/posts/`});
+      res.status(404).render("error",{userInfo: req.session.user, error: e, link:`/posts/`});
     }
   })
   .delete(async (req, res) => {
@@ -78,19 +78,19 @@ router
 
       req.params.id = validation.checkId(req.params.id, 'Comment ID');
     } catch (e) {
-      return res.status(400).render("error",{error: e, link:`/posts/`});
+      return res.status(400).render("error",{userInfo: req.session.user, error: e, link:`/posts/`});
     }
 
     try {
       const comment = await commentsData.getCommentById(req.params.id);
       if (comment === undefined)
-        return res.status(500).render("error",{error: "Internal Service Error", link:`/posts/`});
+        return res.status(500).render("error",{userInfo: req.session.user, error: "Internal Service Error", link:`/posts/`});
 
       const commenterId = comment.user_id;
       const user_id = req.session.user._id;
       let ownComment = commenterId.equals(user_id);
       if (!ownComment) {
-        return res.status(400).render("error",{error: 'Only comment owner can delete a comment', link:`/posts/`});
+        return res.status(400).render("error",{userInfo: req.session.user, error: 'Only comment owner can delete a comment', link:`/posts/`});
       }
       const post_id = comment.post_id;
       await commentsData.removeComment(req.params.id, user_id);
@@ -98,10 +98,10 @@ router
 
     } catch (e) {
       if (Array.isArray(e)) // when removeComment throws
-        res.status(e[0]).render("error",{error: e[1], link:`/posts/`});
+        res.status(e[0]).render("error",{userInfo: req.session.user, error: e[1], link:`/posts/`});
 
       // when getCommentById throws
-      res.status(500).render("error",{error: e, link:`/posts/`});
+      res.status(500).render("error",{userInfo: req.session.user, error: e, link:`/posts/`});
     }
   });
 

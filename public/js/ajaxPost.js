bdb8f2fe53e3e8cc45954711e4932e7587b7f6b2
Tyler Soiferman <twsoiferman@outlook.com>
2023-12-16 19:24:19 -0500

    ajax for post feed

--- public/js/ajaxPost.js
+++ public/js/ajaxPost.js
@@ -1,7 +1,7 @@
-//TEMPLATE, NEEDS UPDATING WHEN PAGES ARE IN!!!!
 (function ($) {
-    let postButton = $('#postButton'),
-    allPosts = $('#all-posts')
+    console.log("GOT HERE!!!")
+    let postButton = $('#post-button'),
+    allPosts = $('#feed')
     postButton.on('click', function (event) {
         let currentUrl = window.location.pathname
         let musicId = currentUrl.split('/music')[1]
@@ -10,9 +10,32 @@
             url: `/music/${musicId}`
         }
         $.ajax(requestConfig).then(function (responseMessage) {
-            let postEl = `<p>Temporary, will replace with actual post html</p>`
-            //assuming the section with the list of posts is called all-posts, will change accordingly
+            console.log("Made it in ajax, here's the msg:")
+            console.log(responseMessage)
+            let postEl = `<div class="post-1">
+            <div class="top">
+                <div class="user">
+                    <div class="info">
+                        <h3><a href="/users/${responseMessage.user_id}">${responseMessage.username}</a></h3>
+                        <small>${responseMessage.date}</small>
+                    </div>
+
+                </div>
+            </div>
+            <div class="content">
+                <p>
+                    ${responseMessage.content}
+                </p>
+            </div>
+            <div class="rating">
+                <h4>Rating: ${responseMessage.rating}/5</h4>
+            </div>
+            <div class="interaction-buttons">
+                <span><i class="uil uil-heart"></i></span>
+                <span><i class="uil uil-comment-alt"></i></span>
+            </div>
+        </div>`
             allPosts.append(postEl)
         })
     })
-})
+})(window.jQuery);
